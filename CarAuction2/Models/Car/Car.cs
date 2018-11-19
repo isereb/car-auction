00127843e4e312d6745a40f4207e41fe62139d0c
isereb <ruxxay.coder@gmail.com>
2018-11-19 13:46:03 -0500

    Now one car can have multiple pictures

--- CarAuction2/Models/Car/Car.cs
+++ CarAuction2/Models/Car/Car.cs
@@ -1,15 +1,26 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CarAuction2.Models.Car
 {
     public class Car
     {
+
         public int CarId { get; set; }
 
+        [Required]
+        [MaxLength(64)]
         public string Mark { get; set; }
+        
+        [Required]
+        [MaxLength(64)]
         public string Model { get; set; }
-        public DateTime ProductionYear { get; set; }
-        public string PathToPicture { get; set; }
-        public CarStatus CarStatus { get; set; }
+        
+        [Required]
+        public int ProductionYear { get; set; }
+
+        public CarPicture Picture { get; set; }
+
+        [Required] public CarStatus CarStatus { get; set; } = CarStatus.OnSale;
     }
 }
---
+++ CarAuction2/Models/Car/CarPicture.cs
@@ -0,0 +1,8 @@
+namespace CarAuction2.Models.Car
+{
+    public class CarPicture
+    {
+        public int PictureId { get; set; }
+        public Car Car { get; set; }
+    }
+}
