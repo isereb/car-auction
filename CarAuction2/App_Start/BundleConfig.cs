5cf6e67ecd45b270eba00b1553155b7e2133182d
isereb <ruxxay.coder@gmail.com>
2018-11-19 15:39:13 -0500

    Updated and simplified bundles

--- CarAuction2/App_Start/BundleConfig.cs
+++ CarAuction2/App_Start/BundleConfig.cs
@@ -7,19 +7,15 @@ namespace CarAuction2
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new StyleBundle("~/styles").Include(
+                "~/Content/styles/bootstrap.css",
+                "~/Content/styles/site.css"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/scripts").Include(
+                "~/Content/scripts/jquery-{version}.js",
+                "~/Content/scripts/bootstrap.js"
+            ));
         }
     }
 }
